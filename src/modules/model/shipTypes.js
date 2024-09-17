03a381e85703d7a3b2a33afba613e15f6371fafe
alexdilion <alexdilion@protonmail.com>
2024-09-17 19:53:21 +0100

    feat: add shipType config

---
+++ src/modules/model/shipTypes.js
@@ -0,0 +1,19 @@
+const shipTypes = {
+    destroyer: {
+        size: 2,
+    },
+    submarine: {
+        size: 3,
+    },
+    cruiser: {
+        size: 3,
+    },
+    battleship: {
+        size: 4,
+    },
+    carrier: {
+        size: 5,
+    },
+};
+
+export default shipTypes;
