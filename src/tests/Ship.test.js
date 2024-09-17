da062c78d0b99130179cc6dbb1f610ffa1e817a6
alexdilion <alexdilion@protonmail.com>
2024-09-17 20:40:56 +0100

    test: add tests for Ship hit method

--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -14,3 +14,31 @@ describe("Ship constructor", () => {
         expect(ship.size).toBe(2);
     });
 });
+
+describe("Ship hit method", () => {
+    test("Hits starts at zero", () => {
+        const ship = new Ship("battleship");
+
+        expect(ship.hits).toBe(0);
+    });
+
+    test("Hits increases after being hit", () => {
+        const ship = new Ship("battleship");
+
+        expect(ship.hits).toBe(0);
+        ship.hit();
+        expect(ship.hits).toBe(1);
+    });
+
+    test("Hits never greater than size", () => {
+        const ship = new Ship("submarine");
+
+        expect(ship.hits).toBe(0);
+
+        for (let i = 0; i < 10; i++) {
+            ship.hit();
+        }
+
+        expect(ship.hits).toBe(3);
+    });
+});
