42812c4dd1e546ba34b24825c7a00d448bddf75e
alexdilion <alexdilion@protonmail.com>
2024-09-21 14:19:43 +0100

    feat: add boundary validation

--- src/modules/model/Gameboard.js
+++ src/modules/model/Gameboard.js
@@ -10,6 +10,25 @@ export default class Gameboard {
         this.#hits = new Array2d(this.#size, this.#size);
     }
 
+    #validatePosition(shipSize, row, column, rotated) {
+        if (!rotated && (column < 0 || shipSize + column > this.#size)) {
+            return false;
+        }
+
+        if (rotated && (row < 0 || shipSize + row > this.#size)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    place(ship, row, column, rotated) {
+        if (!this.#validatePosition(ship.size, row, column, rotated))
+            return false;
+
+        return true;
+    }
+
     get board() {
         return this.#board;
     }
