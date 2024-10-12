593386c353b324d56ecd2d9f1759e7eb2a41ba55
alexdilion <alexdilion@protonmail.com>
2024-10-12 23:41:07 +0100

    test: add receiveAttack tests

--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,6 +1,8 @@
 import Player from "../modules/model/Player";
 import Ship from "../modules/model/Ship";
 
+const ship = new Ship("destroyer");
+
 describe("Player constructor", () => {
     const player = new Player("Alex");
 
@@ -15,7 +17,6 @@ describe("Player constructor", () => {
 
 describe("Ship placement", () => {
     let player = new Player("p1");
-    const ship = new Ship("destroyer");
 
     afterEach(() => (player = new Player("p1")));
 
@@ -25,5 +26,49 @@ describe("Ship placement", () => {
 
     test("Invalid placement", () => {
         expect(player.placeShip(ship, 0, 9, false)).toBe(false);
+    });
+});
+
+describe("Attack method", () => {
+    let p1 = new Player("p1");
+
+    afterEach(() => {
+        p1 = new Player("p1");
+    });
+
+    test("Attack on empty cell", () => {
+        p1.placeShip(ship, 0, 0, false);
+        const outcome = p1.receiveAttack(1, 0);
+
+        expect(outcome).toBe("miss");
+    });
+
+    test("Attack on cell with ship", () => {
+        p1.placeShip(ship, 0, 0, false);
+        const outcome = p1.receiveAttack(0, 0);
+
+        expect(outcome).toBe("hit");
+    });
+
+    test("Attack on last ship cell sinks ship", () => {
+        p1.placeShip(ship, 0, 0, false);
+        p1.receiveAttack(0, 0);
+        const outcome = p1.receiveAttack(0, 1);
+
+        expect(outcome).toBe("sunk");
+        expect(p1.gameboard.isGameOver()).toBe(true);
+    });
+
+    test("Attack on already hit cell", () => {
+        p1.receiveAttack(0, 0);
+        const outcome = p1.receiveAttack(0, 0);
+
+        expect(outcome).toBe(null);
+    });
+
+    test("Attack on invalid cell", () => {
+        const outcome = p1.receiveAttack(100, 100);
+
+        expect(outcome).toBe(null);
     });
 });
